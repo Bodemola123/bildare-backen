4fff98b35bf998d09e60f7279f0c5d2886c2d94e
Bodemola <bodemola123@gmail.com>
2025-11-15 11:40:05 +0100

    Render fix

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // src/index.ts
 import express, { Request, Response, NextFunction, Router } from "express";
-import { PrismaClient, User } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
